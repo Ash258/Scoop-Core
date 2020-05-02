25a72f8fd86fe3174c3b6ae4f773c3fe013c669f
Jakub Čábera <cabera.jakub@gmail.com>
2020-05-02 17:52:56 +0200

    ??

--- test/Scoop-Alias.Tests.ps1
+++ test/Scoop-Alias.Tests.ps1
@@ -1,7 +1,5 @@
 . "$PSScriptRoot\..\lib\Alias.ps1"
 
-reset_aliases
-
 describe 'Add-Alias' -Tag 'Scoop' {
     # BeforeAll {
     mock shimdir { 'TestDrive:\shim' }
@@ -14,10 +12,10 @@ describe 'Add-Alias' -Tag 'Scoop' {
 
     context 'alias does not exist' {
         it 'creates a new alias' {
-            $aliasFile = "$shimdir\scoop-rm.ps1"
+            $aliasFile = "$shimdir\scoop-cosiTest.ps1"
             $aliasFile | Should -Not -Exist
 
-            # Add-Alias -Name 'rm' -Command '"hello, world!"'
+            # Add-Alias -Name 'cosiTest' -Command '"hello, world!"'
             # Invoke-Expression $aliasFile | Should -Be "hello, world!"
         }
     }
