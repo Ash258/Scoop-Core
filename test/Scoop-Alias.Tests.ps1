713ce936cf1de3a467779e6e1e5d37ddf8982845
Jakub Čábera <cabera.jakub@gmail.com>
2020-05-02 17:24:39 +0200

    clean

--- test/Scoop-Alias.Tests.ps1
+++ test/Scoop-Alias.Tests.ps1
@@ -21,38 +21,4 @@ describe 'Add-Alias' -Tag 'Scoop' {
             Invoke-Expression $aliasFile | Should -Be "hello, world!"
         }
     }
-
-    exit
-    context "alias exists" {
-        it "does not change existing alias" {
-            $alias_file = "$shimdir\scoop-rm.ps1"
-            new-item $alias_file -type file
-            $alias_file | should -exist
-
-            add_alias "rm" "test"
-            $alias_file | should -FileContentMatch ""
-        }
-    }
-}
-
-describe "rm_alias" {
-    mock shimdir { "TestDrive:\shim" }
-    mock set_config { }
-    mock get_config { @{ } }
-
-    $shimdir = shimdir
-    mkdir $shimdir
-
-    context "alias exists" {
-        it "removes an existing alias" {
-            $alias_file = "$shimdir\scoop-rm.ps1"
-            add_alias "rm" '"hello, world!"'
-
-            $alias_file | should -exist
-            mock get_config { @(@{"rm" = "scoop-rm" }) }
-
-            rm_alias "rm"
-            $alias_file | should -not -exist
-        }
-    }
 }
