e813adbaf5b08487ad8e6884a2f6e7f9f099ed6d
Jakub Čábera <cabera.jakub@gmail.com>
2020-10-09 13:25:59 +0200

    feat(commands): Allow --help, -h for all commands
    - Closes #69

--- bin/scoop.ps1
+++ bin/scoop.ps1
@@ -8,8 +8,32 @@ Set-StrictMode -Off
 }
 
 Reset-Alias
-$exitCode = 0
 
+$exitCode = 0
+# Powershell automatically bind bash like short parameters as $args, and do not put it in $cmd parameter
+
+# Only -v and --version passed
+$version = ($cmd -eq '--version') -or (!$cmd -and ('-v' -in $args))
+Write-Host "Vesion: ", $version
+
+# Scoop itself help should be shown only if explicitly asked:
+# - No version asked
+# - No command passed
+# - --help, /?, /help, -h passed
+$scoopHelp = !$version -and (($cmd -in @($null, '--help', '/?', '/help')) -or (!$cmd -and ('-h' -in $args)))
+Write-Host "Scoop: ", $scoopHelp
+
+# Valid command execution
+$validCommand = (commands) -contains $cmd
+Write-Host "Command: ", $validCommand
+
+# Command help should be shown only if:
+# - No help for scoop asked
+# - $cmd is passed
+# - --help, -h is in $args
+$commandHelp = !$scoopHelp -and $validCommand -and ($cmd -and (('--help' -in $args) -or ('-h' -in $args)))
+Write-Host "Command Help: ", $commandHelp
+exit 0
 if (('--version' -eq $cmd) -or (!$cmd -and ('-v' -in $args))) {
     Write-UserMessage -Message 'Current Scoop (soon to be Shovel) version:' -Output
     Invoke-GitCmd -Command 'VersionLog' -Repository (versiondir 'scoop' 'current')
