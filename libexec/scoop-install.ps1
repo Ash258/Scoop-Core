9e8d976548f7b193707820d1ea7089dfd3e6b6cf
Jakub Čábera <cabera.jakub@gmail.com>
2020-12-01 12:07:31 -0800

    fix(install): Installation of older versions over latest one

--- libexec/scoop-install.ps1
+++ libexec/scoop-install.ps1
@@ -152,7 +152,14 @@ foreach ($app in $apps) {
 
     $cleanApp, $bucket = parse_app $app
 
-    if (is_installed $cleanApp $global) { continue }
+    # Prevent checking of already installed applications if specific version was provided.
+    # In this case app will be fullpath to the manifest in \workspace folder and specific version will contains <app>@<version>
+    # Allow to install zstd@1.4.4 after 1.4.5 was installed before
+    Write-Host $specific_versions -f magenta
+    Write-Host $app -f green
+    if ($specific_versions -contains $app) {
+        if (is_installed $cleanApp $global) { continue }
+    }
 
     # Install
     try {
