264c3593be70da30fb542d3065989592509d1ed1
Jakub Čábera <cabera.jakub@gmail.com>
2020-12-31 19:51:50 +0100

    cleanup

--- libexec/scoop-install.ps1
+++ libexec/scoop-install.ps1
@@ -134,8 +134,6 @@ $apps = ensure_none_failed $apps $global
 
 if ($apps.Count -eq 0) { Stop-ScoopExecution -Message 'Nothing to install' }
 
-Write-Host $apps -f yellow
-# exit 0
 $apps, $skip = prune_installed $apps $global
 
 $skip | Where-Object { $explicit_apps -contains $_ } | ForEach-Object {
@@ -161,7 +159,6 @@ foreach ($app in $apps) {
 
     $cleanApp, $bucket = parse_app $app
 
-    # shovel install zstd@1.4.3 .\alfa.json https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/bat.json
     # Prevent checking of already installed applications if specific version was provided.
     # In this case app will be fullpath to the manifest in \workspace folder and specific version will contains <app>@<version>
     # Allow to install zstd@1.4.4 after 1.4.5 was installed before
