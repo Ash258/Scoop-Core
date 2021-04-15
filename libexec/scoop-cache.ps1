77da14c0e8aa69ad0313174c147bbf80fe5f50b4
Jakub Čábera <cabera.jakub@gmail.com>
2021-04-15 17:00:28 +0200

    implement rm

--- libexec/scoop-cache.ps1
+++ libexec/scoop-cache.ps1
@@ -31,6 +31,7 @@ $cmd = if ($arguments[0]) { $arguments[0] } else { 'show' }
 $isShow = $cmd -eq 'show'
 $applications = $arguments[1..($arguments.Count)]
 $exitCode = 0
+$problems = 0
 
 if ($cmd -notin @('rm', 'show')) { Stop-ScoopExecution -Message "Unknown subcommand: '$cmd'" -Usage (my_usage) }
 if (!$isShow -and !$applications) { Stop-ScoopExecution -Message 'Parameter <apps> missing' -Usage (my_usage) }
@@ -39,17 +40,16 @@ if ($isShow) {
     Show-CachedFileList $applications
 } else {
     foreach ($app in $applications) {
-        continue
+        try {
+            Join-Path $SCOOP_CACHE_DIRECTORY "$app#*" | Remove-Item -Force -Recurse
+            Join-Path $SCOOP_CACHE_DIRECTORY "$app.txt" | Remove-Item -ErrorAction 'SilentlyContinue' -Force -Recurse
+        } catch {
+            Write-UserMessage -Message "Removing ${app}: $($_.Exception.Message)" -Err
+            ++$problems
+        }
     }
 }
-exit 258
 
-switch ($cmd) {
-    'rm' {
-        if (!$app) { Stop-ScoopExecution -Message 'Parameter <app> missing' -Usage (my_usage) }
-        Join-Path $SCOOP_CACHE_DIRECTORY "$app#*" | Remove-Item -Force -Recurse
-        Join-Path $SCOOP_CACHE_DIRECTORY "$app.txt" | Remove-Item -ErrorAction 'SilentlyContinue' -Force -Recurse
-    }
-}
+if ($problems -gt 0) { $exitCode = 10 + $problems }
 
 exit $exitCode
