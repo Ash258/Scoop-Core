e6cd6fbf0dd60082c155903737e45a999c98314a
Jakub Čábera <cabera.jakub@gmail.com>
2021-04-25 15:48:00 +0200

    polish utils

--- libexec/scoop-utils.ps1
+++ libexec/scoop-utils.ps1
@@ -1,24 +1,23 @@
 # Usage: scoop utils <UTILITY> [<OPTIONS>] [<PATH>] [--additional-options <OPTS>...]
 # Summary: Wrapper around utilities for maintaining buckets and manifests.
-# Help: Bucket maintainers no longer need to have own 'bin' folder and they can use native command.
+# Help: Bucket maintainers do need to have own 'bin' folder and they can use native command.
 #
-# Two possible ways how to pass parameters to this command:
-#     1. Pass fullname of the manifest. No wildcard support
-#     2. Pass simple string (with wildcard support) to search in ./bucket/ folder
-#           Or pass --bucketdir to override default bucket path
+# There are two possible ways how to pass parameters to this command:
+#     1. Pass fullname of the manifest. This will check explicitly the provided path with no wildcard support.
+#     2. Pass simple string (with wildcard support) to search in ./bucket/ folder.
+#           Or pass --bucketdir to override default bucket path.
 #
 # Options:
+#   -h, --help              Show help for this command.
 #   -b, --bucketdir         Use specific bucket directory instead of default './bucket/'.
 #   --additional-options    Valid, powershell-like parameters passed to specific utility binary.
 #                               Refer to each utility for all available parameters/options.
-#   -h, --help              Show help for this command.
 #
 # Example usage:
 #    'scoop utils checkver $env:SCOOP\buckets\main\bucket\pwsh.json' => Check explicitly passed manfiest files
 #    'scoop utils checkver manifest*' => Check all manifests matching manifest* in ./bucket/
 #    'scoop utils checkhashes manifest*' --bucketdir ..\..\testbucket\bucket => Check all manifests matching manifest* in provided directory
-#    'scoop utils auto-pr --additional-options -Upstream "user/repo:branch" -skipcheckver -push' => Execute auto-pr with specific upstream string
-#
+#    'scoop utils auto-pr --additional-options -Upstream "user/repo:branch" -Skipcheckver -Push' => Execute auto-pr utility with specific upstream string
 
 'core', 'getopt', 'Helpers', 'help' | ForEach-Object {
     . (Join-Path $PSScriptRoot "..\lib\$_.ps1")
@@ -29,15 +28,15 @@ $AdditionalArgs = @()
 # Remove additional args before processing arguments
 if ($args -contains '--additional-options') {
     $index = $args.IndexOf('--additional-options')
-    $getopt = $args[0..($index - 1)]
-    $AdditionalArgs = $args[($index + 1)..($args.Count - 1)]
+    $getopt = $args[0..($index - 1)] # Everything before --additional-options is considered as wrapper command options.
+    $AdditionalArgs = $args[($index + 1)..($args.Count - 1)] # Everything after is considered as utility-specific option.
 }
 
 #region Parameter handling/validation
-$opt, $rem, $err = getopt $getopt 'b:' 'bucketdir='
+$Opt, $Rem, $err = getopt $getopt 'b:' 'bucketdir='
 if ($err) { Stop-ScoopExecution -Message "scoop utils: $err" -ExitCode 1 -Usage (my_usage) }
 
-$Utility = $rem[0]
+$Utility = $Rem[0]
 $VALID_UTILITIES = @(
     'auto-pr'
     'checkhashes'
@@ -49,19 +48,19 @@ $VALID_UTILITIES = @(
 )
 
 if (!$Utility) { Stop-ScoopExecution -Message 'Parameter <UTILITY> missing' -ExitCode 1 -Usage (my_usage) }
-if ($Utility -notin $VALID_UTILITIES) { Stop-ScoopExecution -Message "'$Utility' is not valid Scoop utility" -ExitCode 1 -Usage (my_usage) }
+if ($Utility -notin $VALID_UTILITIES) { Stop-ScoopExecution -Message "'$Utility' is not valid scoop utility" -ExitCode 1 -Usage (my_usage) }
 
 $UtilityPath = (Join-Path $PSScriptRoot '..\bin' | Get-ChildItem -Filter "$Utility.ps1" -File).FullName
 $BucketFolder = Join-Path $PWD 'bucket'
+$ManifestPath = $Rem[1]
 
-if ($opt.b -or $opt.bucketdir) { $BucketFolder = $opt.b, $opt.bucketdir | Where-Object { $null -ne $_ } | Select-Object -First 1 }
+if ($Opt.b -or $Opt.bucketdir) { $BucketFolder = $Opt.b, $Opt.bucketdir | Where-Object { $null -ne $_ } | Select-Object -First 1 }
 
-$ManifestPath = $rem[1]
-# Edge case for fullpath or nothing, which needed specific handling
+# Edge case for fullpath or nothing, which needs special handling.
 if (!$ManifestPath) {
     $ManifestPath = '*'
-} elseif (Test-Path -LiteralPath $ManifestPath -ErrorAction 'SilentlyContinue') {
-    $item = Get-Item $ManifestPath
+} elseif (Test-Path -LiteralPath $ManifestPath) {
+    $item = Get-Item -LiteralPath $ManifestPath
     $BucketFolder = $item.Directory.FullName
     $ManifestPath = $item.BaseName
 }
@@ -73,14 +72,14 @@ try {
 }
 #endregion Parameter handling/validation
 
-$exitCode = 0
+$ExitCode = 0
 
 try {
     & $UtilityPath -App $ManifestPath -Dir $BucketFolder @AdditionalArgs
-    $exitCode = $LASTEXITCODE
+    $ExitCode = $LASTEXITCODE
 } catch {
     Write-UserMessage -Message "Utility issue: $($_.Exception.Message)" -Err
-    $exitCode = 3
+    $ExitCode = 3
 }
 
-exit $exitCode
+exit $ExitCode
